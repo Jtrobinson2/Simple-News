c89847d077766ee1c70073619f1f58fe85198515
Jakari Robinson <jakarir2020@gmail.com>
2020-07-14 10:01:04 -0400

    Created news data model class

--- app/src/main/java/com/example/simplenews/models/News.java
+++ app/src/main/java/com/example/simplenews/models/News.java
@@ -1,6 +1,63 @@
 package com.example.simplenews.models;
+
 /*
-* News class objects contain the data fetched from the news api. This is the base data model
-* */
+ * News class objects contain the data fetched from the news api. This is the base data model
+ * */
 public class News {
+    private String imageUrl;
+    private String sourceNewsOutlet;
+    private String description;
+    private String articleUrl;
+    private String publishDate;
+
+
+    /*POJO Constructor*/
+    public News(String imageUrl, String sourceNewsOutlet, String description, String articleUrl, String publishDate) {
+        this.imageUrl = imageUrl;
+        this.sourceNewsOutlet = sourceNewsOutlet;
+        this.description = description;
+        this.articleUrl = articleUrl;
+        this.publishDate = publishDate;
+    }
+
+    /*Getters and setters*/
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getSourceNewsOutlet() {
+        return sourceNewsOutlet;
+    }
+
+    public void setSourceNewsOutlet(String sourceNewsOutlet) {
+        this.sourceNewsOutlet = sourceNewsOutlet;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getArticleUrl() {
+        return articleUrl;
+    }
+
+    public void setArticleUrl(String articleUrl) {
+        this.articleUrl = articleUrl;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
 }
---
+++ app/src/main/java/com/example/simplenews/utils/NetworkUtils.java
@@ -0,0 +1,5 @@
+package com.example.simplenews.utils;
+
+public class NetworkUtils {
+
+}
